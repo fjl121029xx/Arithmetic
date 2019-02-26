391cfed1e3c921c7d326f3c8fe9260c96553bc98
e <sishengqikuo_xx@163.com>
2019-02-26 18:14:21 +0800

    2019-02-26   ...1

---
+++ src/com/li/search/BinarySearch.java
@@ -0,0 +1,42 @@
+package com.li.search;
+
+import java.util.Random;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        // 0 1 2 3 4 5 6 7 8 9 10 11
+        int[] arr = new int[100000];
+        Random r = new Random();
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = i + r.nextInt(10);
+        }
+
+        int waitFind = 99983;
+
+        int i = binarySearch(waitFind, 0, arr.length, arr);
+
+        System.out.println(i);
+
+    }
+
+    public static int binarySearch(int aim, int begin, int end, int[] arr) {
+
+        if (begin <= end) {
+
+            int midIndex = (end + begin) / 2;
+
+            if (aim == arr[midIndex]) {
+
+                return midIndex;
+            } else if (aim > arr[midIndex]) {
+
+                return binarySearch(aim, midIndex + 1, end, arr);
+            } else {
+                return binarySearch(aim, begin, midIndex - 1, arr);
+            }
+        }
+        return -1;
+    }
+}
