ca0dbedf9fc658c33e4bb6b22d7bf061ceb6b64a
fjl121029xx <sishengqikuo_xx@163.com>
2019-05-09 17:29:09 +0800

    2019-05-09    ...1

---
+++ src/com/li/java/HashMapInfiniteLoop.java
@@ -0,0 +1,36 @@
+package com.li.java;
+
+import java.util.HashMap;
+
+public class HashMapInfiniteLoop {
+
+    private static HashMap<Integer, String> map = new HashMap<>(2, 0.75f);
+
+    public static void main(String[] args) {
+        map.put(5, "C");
+
+        while (true) {
+            new Thread("Thread1") {
+                public void run() {
+                    map.put(7, "B");
+                    System.out.println(map);
+                }
+
+                ;
+            }.start();
+            new Thread("Thread2") {
+                public void run() {
+                    map.put(3, "A");
+                    System.out.println(map);
+                }
+
+                ;
+            }.start();
+        }
+
+    }
+
+    static public void o() {
+
+    }
+}
